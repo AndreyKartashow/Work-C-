97a50d5076f37224500ee283f9fc2f0a84e512fb
Andrey <patron6239@gmail.com>
2022-06-26 22:03:39 +0300

    add folder homework

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        // Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее. (a = 5; b = 7 -> max = 7) (a = 2 b = 10 -> max = 10) (a = -9 b = -3 -> max = -3)
+        static void Main(string[] args)
+        {
+            Console.WriteLine ("Привет, эта программа пожет тебе сравнить 2 числа и найти наибольшее");
+            Console.WriteLine ("Введи пожалуйста первое число");
+            // 1. Блок ввода входных данных
+            double number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine ("Введи пожалуйста второе число");
+            double number2 = Convert.ToInt32(Console.ReadLine());
+            // 2. Блок сравнения
+            double max = number1;
+            if (number1 > number2)
+            {
+                Console.WriteLine (max);
+            }
+            else
+            {
+                max = number2;
+                Console.WriteLine ($"Наибольшее число: {max}");
+            }
+        }
+    }
+}
