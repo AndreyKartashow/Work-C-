db269a908700fe306b3c57f1b26e89aab5445f7f
Andrey <patron6239@gmail.com>
2022-06-26 16:31:20 +0300

    Add filder Ex1

---
+++ Ex1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        // Напишите программу, которая на вход принимает число и выдает его квадрат
+        static void Main(string[] args)
+        {
+            Console.WriteLine ("Введите целое число");
+            // 1. Получаем число
+            int number = Convert.ToInt32(Console.ReadLine());
+            // 2. Умножаем число
+            int square = number * number;
+            // или получаем квадрат в переменной number следующим способом  "number*=number"
+            // 3. Вывод результата
+            Console.WriteLine ($"Квадрат числа: {square}");
+        }
+    }
+}
