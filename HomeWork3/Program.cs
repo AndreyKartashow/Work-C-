980400bd8a9544589f4df978e8eff7ca021832f0
Andrey <patron6239@gmail.com>
2022-06-27 13:47:48 +0300

    Add folder HomeWork3

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        // Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+        static void Main(string[] args)
+        {
+            Console.WriteLine ("Привет, эта программа пожет тебе определить, является ли число четным или нет");
+        // 1. Блок ввода входных данных
+            Console.WriteLine ("Введи пожалуйста целое число");
+            double number = Convert.ToInt32(Console.ReadLine());
+        // 2. Блок действия
+            double remainder = number % 2;  
+        // 3. Блок условия                
+            if (remainder == 0)
+            {
+                Console.WriteLine ("Число является четным");     
+            }
+            else 
+            {
+                Console.WriteLine ("Число не является четным");  
+            }
+        }
+    }
+}
