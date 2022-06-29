a287fde33d7b6124abcb19cf2b7012c7b85c680f
Andrey <patron6239@gmail.com>
2022-06-29 20:32:17 +0300

    Add HomeWork 6

---
+++ HomeWork6/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        // Напишите программу, которая на вход принимает целое число и выдает его третью цифру или сообщает что его нет
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число");
+            // 1. Получаем из консоли число в типе данных "строка"
+            string str = Console.ReadLine();
+            int size = str.Length;
+            int index = 0;
+            // 2. Производим проверку по третьй цифре
+            if (size < 3)
+                {
+                    Console.WriteLine("Третьей цифры нет");
+                }
+            else 
+                {
+                    Console.WriteLine(str [2]);
+                }
+        }
+    }
+}
