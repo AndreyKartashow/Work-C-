a23a1e2419e76ec16ed0f5bb4ab3da0e68c9ccd3
Andrey <patron6239@gmail.com>
2022-06-29 19:58:03 +0300

    add homework5

---
+++ HomeWork5/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        // Напишите программу, которая на вход принимает трехзначное число и выдает его вторую цифру этого числа
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое трехзначное число");
+            // 1. Получаем из консоли число в типе данных "строка"
+            string str = Console.ReadLine();
+            // 2. Выводим вторую цифру 
+            Console.WriteLine(str [1]);
+        }
+    }
+}
