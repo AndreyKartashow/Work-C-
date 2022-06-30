6a39710b7ab8f03d8fa11512ea105e3678f46871
Andrey <patron6239@gmail.com>
2022-06-30 13:43:49 +0300

    add folder homework 7

---
+++ HomeWork7/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        // Напишите программу, которая на вход принимает целое число и выдает его третью цифру или сообщает что его нет
+        static void Main(string[] args)
+        {
+            // 1. Создаем массив на 7 дней
+            string[] ArrayWeek = { "Такого дня недели нет", "понедельник", "вторник", "среда", "четверг", "пятница", "суббота-выходной", "воскресенье-выходной" };
+            // 2. Просим ввести число, которое будет соответсвовать дню недели
+            Console.WriteLine("Введите пожалуйста число и я подскажу какой день недели ему соответсвтвует");
+            int number = Convert.ToInt32(Console.ReadLine());
+            // 3. В цикле производим проверку на соответсвие введеного числа из консоли и нашего элемента массива
+            int index = 0;
+            int size = ArrayWeek.Length;
+            if (number > size | number < 0)
+            {
+                Console.WriteLine("Вы ввыели неверное число");
+            }
+            else
+            {
+                while (index < size)
+                {
+                    if (number == index)
+                    {
+                        Console.WriteLine(ArrayWeek[index]);
+                        break;
+                    }
+                    index++;
+                }
+            }
+        }
+    }
+}
