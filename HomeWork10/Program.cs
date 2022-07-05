08e2f656764c862e09a4f69e67c2c56c6c711052
Andrey <patron6239@gmail.com>
2022-07-05 20:07:18 +0300

    Add HomeWork10

---
+++ HomeWork10/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace GeekBrains
+{
+    class Program
+    {
+        // Напишите программу, которая на вход принимает число и выдает таблицу кубов от 1 до N
+        static void Main(string[] args)
+        {
+            Console.WriteLine ("Введите целое число");
+            // 1. Получаем число
+            int number = Convert.ToInt32(Console.ReadLine());
+            // 2. Выводим таблицу кубов в цикле 
+            for (int i = 1; i < number; i++)
+            Console.WriteLine($"{i} x {i} x {i} = {i*i*i}");
+        }
+    }
+}
