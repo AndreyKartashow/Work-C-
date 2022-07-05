813462f4816bf12ecdb656f72c590c5ca0cb80e8
Andrey <patron6239@gmail.com>
2022-07-05 20:59:44 +0300

    add HomeWork9

---
+++ HomeWork9/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        // Напишите программу, которая принимает на вход координаты двух точек 
+        // и находит расстояние между ними в 3D пространстве.
+        static void Main(string[] args)
+        {
+            Console.WriteLine ("Введите координату X точки А");
+            double Xa = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine ("Введите координату Y точки А");
+            double Ya = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine ("Введите координату Z точки А");
+            double Za = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine ("Введите координату X точки B");
+            double Xb = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine ("Введите координату Y точки B");
+            double Yb = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine ("Введите координату Z точки B");
+            double Zb = Convert.ToDouble(Console.ReadLine());
+            // 1. Получаем число
+            double length = Math.Sqrt((Xb-Xa)*(Xb-Xa)+(Yb-Ya)*(Yb-Ya)+(Zb-Za)*(Zb-Za));
+            Console.WriteLine ($"Расстояние между точками: {length}");
+        }
+    }
+}
+
+
+
