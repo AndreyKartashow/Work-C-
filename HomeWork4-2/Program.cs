4fc052ef5c20f7d7b6a1b19069a37348ec5bfcf0
Andrey <patron6239@gmail.com>
2022-07-06 21:29:35 +0300

    add home_work_4-2

---
+++ HomeWork4-2/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace GeekBrains
+{
+    class Program
+    {
+        // Напишите программу, которая принимает на вход число и выдает сумму цифр в числе
+        static void Main(string[] args)
+        {
+            Resume name = new Resume();           
+            Console.WriteLine($"Привет {name.nameUser}");
+            Console.WriteLine("Сейчас "  + DateTime.Now);
+            for ( ; ; )
+            {
+            Console.WriteLine ("Эта программа предназначенна для определения суммы элементов в введеном числе");
+            Console.WriteLine ("Пожалуйста, введите число");
+            int summ = ForeachNumberSumm();
+            Console.WriteLine($"Сумма элементов числа составляет {summ}"); 
+            Console.WriteLine ("Для выхода из программы зажмите клавиши <Ctrl>+<C>");
+            }
+        }
+        // Метод перебора числа и определения суммы его элементов
+        static int ForeachNumberSumm ()
+        {
+            string collection = Console.ReadLine();
+            int result = 0;
+            foreach (char number in collection)
+            {
+                // Преобразование символа "number" в строку и затем в число
+                int numberToInt = int.Parse(Convert.ToString(number));
+                result = result + numberToInt;
+            }
+            return result; 
+        }
+    }
+    class Resume
+    {
+        public string nameUser = "User";
+    }
+}
