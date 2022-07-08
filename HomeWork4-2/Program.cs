b68598ed1cf69820a69ab5f245ae5625c97e1a6c
Andrey <patron6239@gmail.com>
2022-07-08 21:11:33 +0300

    Change folder HomeWork_4-2

--- HomeWork4-2/Program.cs
+++ HomeWork4-2/Program.cs
@@ -5,24 +5,28 @@
         // Напишите программу, которая принимает на вход число и выдает сумму цифр в числе
         static void Main(string[] args)
         {
+            ConsoleKeyInfo esc = new ConsoleKeyInfo();
             Resume name = new Resume();           
             Console.WriteLine($"Привет {name.nameUser}");
             Console.WriteLine("Сейчас "  + DateTime.Now);
-            for ( ; ; )
+            do
             {
-            Console.WriteLine ("Эта программа предназначенна для определения суммы элементов в введеном числе");
-            Console.WriteLine ("Пожалуйста, введите число");
-            int summ = ForeachNumberSumm();
-            Console.WriteLine($"Сумма элементов числа составляет {summ}"); 
-            Console.WriteLine ("Для выхода из программы зажмите клавиши <Ctrl>+<C>");
+                Console.WriteLine ("Эта программа предназначенна для определения суммы элементов в введеном числе");
+                Console.WriteLine ("Пожалуйста, введите число");
+                int summ = ForeachNumberToSumm();
+                Console.WriteLine($"Сумма элементов числа составляет {summ}"); 
+                Console.WriteLine ("Для выхода из программы нажмите клавишу <Esc>, или любую другую если хотите продолжить");
+                esc = Console.ReadKey();
+                Console.Clear();
             }
+            while (esc.Key != ConsoleKey.Escape);
         }
         // Метод перебора числа и определения суммы его элементов
-        static int ForeachNumberSumm ()
+        static int ForeachNumberToSumm ()
         {
-            string collection = Console.ReadLine();
+            string collection = Console.ReadLine()!;
             int result = 0;
-            foreach (char number in collection)
+            foreach (char number in collection!)
             {
                 // Преобразование символа "number" в строку и затем в число
                 int numberToInt = int.Parse(Convert.ToString(number));
