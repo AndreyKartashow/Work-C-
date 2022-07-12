26a0af90daa29e84d46e6ff2d59359d7d893671e
Andrey <patron6239@gmail.com>
2022-07-12 21:30:04 +0300

    change folder HomeWork_6-1 and file program

---
+++ HomeWork-6-1/Program.cs
@@ -0,0 +1,26 @@
+﻿using MethodsImplementatHomeWork;
+
+namespace ImplementationHomeWork
+{
+    class Task1
+    {
+        // Пользователь вводит с клавиатуры M чисел. 
+        // Посчитайте, сколько чисел больше 0 ввёл пользователь.
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo esc = new ConsoleKeyInfo();
+            while (esc.Key != ConsoleKey.Escape)
+            {
+                Console.WriteLine ("Эта програма поможет посчитать количество положительных чисел");
+                Console.WriteLine ("Пожалуйста, введите набор чисел через пробел");
+                string[] arrayNumberString = Console.ReadLine().Split(' ');
+                double[] arrayNumberDouble = MethodsTask1.ArrayStringToArrayInt(arrayNumberString);
+                int result = MethodsTask1.CountPositivePositionArray(arrayNumberDouble);       
+                Console.WriteLine ($"Вы ввели следующее количество положительных чисел: {result}");
+                Console.WriteLine ("Нажмите клавишу <Esc> для выхода или любую другую для продолжения");
+                esc = Console.ReadKey();
+                Console.Clear();
+            }
+        }   
+    }
+}
